5e175ba5712aba9b8a3add5ce68187ac9328e1d9
Kiryl Tymanovich <tymanovich17@gmail.com>
2026-03-01 21:17:28 +0300

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Привет!" in response.data
+
+def test_status_endpoint(client):
+    response = client.get("/status")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["status"] == "ok"
+    assert "message" in data
